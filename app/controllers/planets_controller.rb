927888da629529f5ffbd5366656d160f5fcc696b
Ezgi Seyis <ezgicay_1993@hotmail.com>
2019-01-28 17:31:56 +0100

    Add new view and create action for planet, required authentication for actions

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -1,18 +1,23 @@
 class PlanetsController < ApplicationController
+  before_action :authenticate_user!, except: [ :index, :show]
 
   def new
     @planet = Planet.new
   end
 
   def create
-    @planet = Planet.create(planet_params)
-    redirect_to planets_path
+    @planet = Planet.new(planet_params)
+    if @planet.save!
+      redirect_to planet_path(@planet)
+    else
+      render :new
+    end
   end
 
 
   private
 
   def planet_params
-    params.require(:planet).permit(:name, :description, :solar_system, :category, :size, :price, :photo)
+    params.require(:planet).permit(:name, :description, :solar_system, :category, :size, :price, :photo, :user_id)
   end
 end
