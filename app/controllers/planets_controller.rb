20fbce5d5a0c5d16dd162b49e5f4a59400ce9913
khaled Al-Ammari <khaled99@live.de>
2019-01-28 16:46:38 +0100

    create app/view/show.html.erb

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -1,6 +1,14 @@
 class PlanetsController < ApplicationController
+  before_action :set_planet, only: [:show]
 
   def show
+  end
 
+  def planet_params
+    params.require(:planet).permit(:name)
+  end
+
+  def set_planet
+    @planet = Planet.find(params[:id])
   end
 end
