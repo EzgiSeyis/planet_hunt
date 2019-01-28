32be92c83b44ca8020e2c37e29e1653a8c1e4fa5
Ezgi Seyis <ezgicay_1993@hotmail.com>
2019-01-28 16:48:04 +0100

    saving for master merge

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -1,2 +1,18 @@
 class PlanetsController < ApplicationController
+
+  def new
+    @planet = Planet.new
+  end
+
+  def create
+    @planet = Planet.create(planet_params)
+    redirect_to planets_path
+  end
+
+
+  private
+
+  def planet_params
+    params.require(:planet).permit(:name, :description, :solar_system, :category, :size, :price, :photo)
+  end
 end
