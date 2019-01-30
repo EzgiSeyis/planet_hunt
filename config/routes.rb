7a139a6f901737845a7bbcfdafc683f01ff0c87a
Christian Uhmann <uhmann@outlook.de>
2019-01-30 15:10:29 +0100

    Booking status Owner on Dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
     resources :bookings, only: [:new, :create]
   end
   root to: 'planets#index'
+  get 'dashboard', to: 'dashboards#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
