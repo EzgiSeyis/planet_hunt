59ce1892639c3fd22dca133864bcdd37b3a8e8cf
khaled Al-Ammari <khaled99@live.de>
2019-01-30 14:53:42 +0100

    Add view-controller, view-policy and view-new Action

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'reviews/create'
   devise_for :users
   resources :planets do
     resources :bookings, only: [:new, :create]
   end
+
+  resources :bookings, only: [] do
+    resources :reviews, only: [:new, :create]
+  end
   root to: 'planets#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+#bookings/id/Reviews/new...
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_30_111713) do
+ActiveRecord::Schema.define(version: 2019_01_30_131225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,10 @@ ActiveRecord::Schema.define(version: 2019_01_30_111713) do
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "booking_id"
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -67,4 +71,6 @@ ActiveRecord::Schema.define(version: 2019_01_30_111713) do
   add_foreign_key "bookings", "planets"
   add_foreign_key "bookings", "users"
   add_foreign_key "planets", "users"
+  add_foreign_key "reviews", "bookings"
+  add_foreign_key "reviews", "users"
 end
