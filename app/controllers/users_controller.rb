0c11889c1c052da54d8a12e467ebc183c759ffa4
Ezgi Seyis <ezgicay_1993@hotmail.com>
2019-01-30 14:47:05 +0100

    My profile user added and description text area added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,25 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    authorize @user
   end
+
+  def update
+    @user = User.find(params[:id])
+    authorize @user
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+    else
+      render :show
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:description)
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     resources :bookings, only: [:new, :create]
   end
 
-  resources :users, only: [:show]
+  resources :users, only: [:show, :update]
   root to: 'planets#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
