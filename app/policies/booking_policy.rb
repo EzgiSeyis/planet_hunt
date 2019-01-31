2dfa18ae6b606b85a408878c5b6f043a46e1fc75
khaled Al-Ammari <khaled99@live.de>
2019-01-31 13:45:05 +0100

    Book now Butten finish

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -10,7 +10,7 @@ class BookingPolicy < ApplicationPolicy
   end
 
   def create?
-    true
+    record.planet.user != user # user == ist der Curent user . Check ob der Plannet User = curennt user
   end
 
   def accept?
